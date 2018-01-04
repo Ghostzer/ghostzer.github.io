2a1cca0dc162825affdf855800ccfbe882548d6c
Ghostzer <djxrico@gmail.com>
2018-01-04 11:13:42 +0100

    Update V3

--- assets/js/git.js
+++ assets/js/git.js
@@ -3,7 +3,7 @@ jQuery.githubUser = function(username, callback) {
 }
  
 jQuery.fn.loadRepositories = function(username) {
-    this.html("<h2 id='recup'>Récupération des repos en cours, merci de patienter.</h2>");
+    this.html("<h2 id='recup'>Please wait while we retrieve repos...</h2>");
      
     var target = this;
     $.githubUser(username, function(data) {
@@ -14,9 +14,9 @@ jQuery.fn.loadRepositories = function(username) {
         target.empty().append(list);
         $(repos).each(function() {
             if (this.name != (username.toLowerCase()+'.github.com')) {
-                list.append('<div class="box"><div class="box-content"><span class="titre_projets">' 
-                    + this.name + '</span> <em>'+(this.language?('('+this.language+')'):'')+'</em>'+'<p>' + this.description +''
-                    +'<div class="box-footer"><a href="'+ (this.homepage?this.homepage:this.html_url) +'" class="button">Voir le projet<div>');
+                list.append('<div class="box"><div class="box-content"> < <a class="project_title" href="'+ (this.homepage?this.homepage:this.html_url) +'"><span>' 
+                    + this.name + '</span></a> /> <em>'+(this.language?('('+this.language+')'):'')+'</em>'+'<p>' + this.description +''
+                    +'');
             }
         });      
       });
